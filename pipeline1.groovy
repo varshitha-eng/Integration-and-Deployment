f97eceda7bc1dc0119be7890c073f2ac43a01cac
varshitha-eng <55194030+varshitha-eng@users.noreply.github.com>
2024-05-11 16:58:32 +1000

    Update pipeline1.groovy

--- pipeline1.groovy
+++ pipeline1.groovy
@@ -4,7 +4,7 @@ pipeline{
     stages{
         stage('Build'){
             steps{
-                echo "Building the code using Gradle Tool, to compile and package the code"
+                echo "The code is built using Gradle Tool, to compile and package the code"
             }
         }
 
